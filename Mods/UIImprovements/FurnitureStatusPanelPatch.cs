f17035ed749257b0f607499eeef42c10ecdf67dd
Witchy <witchymods@gmail.com>
2020-07-20 09:19:03 -0400

    version 1.1.1

---
+++ Mods/UIImprovements/FurnitureStatusPanelPatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace WitchyMods.UIImprovements
+{
+    [HarmonyPatch(typeof(FurnitureStatusPanel), "SetSelectedFurniture")]
+    public class FurnitureStatusPanel_SetSelectedFurniturePatch
+    {
+        public static void Postfix(FurnitureStatusPanel __instance, Furniture furniture)
+        {
+            UIImprovementsMod.Instance.SelectedFurniture = furniture;
+
+            if (furniture != null)
+            {
+                bool marked = (bool)AccessTools.Field(typeof(Furniture), "isMarkedForDeconstruction").GetValue(furniture);
+                __instance.destroyFurnitureButton.image.color = (marked ? Color.black : Color.white);
+            }
+        }
+    }
+}
--- Mods/UIImprovements/UIImprovementsMod.cs
+++ Mods/UIImprovements/UIImprovementsMod.cs
@@ -21,6 +21,15 @@ namespace WitchyMods.UIImprovements
         [NonSerialized]
         public static UIImprovementsMod Instance = null;
 
+        [NonSerialized]
+        private NotificationItem _MigrantNotification = null;
+
+        [NonSerialized]
+        private String _LastText = null;
+
+        [NonSerialized]
+        private String _TimerTextTemplate = null;
+
         public override void Load()
         {
             Harmony harmony = new Harmony("UIImprovements");
@@ -30,10 +39,51 @@ namespace WitchyMods.UIImprovements
         public override void Start()
         {
             Instance = this;
+            _TimerTextTemplate = Localization.GetText("witchy_UIImprovements_MigrationTimer");
         }
 
         public override void Update()
         {
+            String timeText = GetNotificationText();
+
+            if (timeText != _LastText)
+            {
+                String text = String.Format(_TimerTextTemplate, timeText);
+
+                if (_MigrantNotification != null && _MigrantNotification.isActiveAndEnabled)
+                    _MigrantNotification.SetText(text);
+
+                else if (ReadyForMigrant())
+                    _MigrantNotification = new NotificationConfig("witchy_newMigrant", text, UISound.NotificationGeneric, null, new Func<bool>(this.IsNotificationObsolete), null, -1, false).Show();
+
+                else
+                {
+                    _LastText = null;
+                    return;
+                }
+
+                _LastText = timeText;
+            }
+        }
+
+        private bool ReadyForMigrant()
+        {
+            HumanManager hManager = WorldScripts.Instance.humanManager;
+            return (hManager.AreExpectationsMet() &&
+                !hManager.HasTooManyColonists() &&
+                WorldScripts.Instance.furnitureFactory.GetModules<AttentionModule>().Any<AttentionModule>(x => x.lit));
+        }
+
+        private bool IsNotificationObsolete()
+        {
+            return WorldScripts.Instance.incidentManager.currentEvents.Any<GameEvent>(x => x.GetEventType() == GameEventType.Migrant) || !ReadyForMigrant();
+        }
+
+        private String GetNotificationText()
+        {
+            IncidentManager incidentManager = WorldScripts.Instance.incidentManager;
+            string second = Mathf.FloorToInt(incidentManager.timeToNextMigrant / 60f) + ":" + Mathf.FloorToInt(incidentManager.timeToNextMigrant % 60f).ToString("D2");
+            return second;
         }
     }
 }
