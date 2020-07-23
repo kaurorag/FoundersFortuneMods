f8201021543c2c6a7e7f456c11899fc2d3c04b7f
Witchy <witchymods@gmail.com>
2020-07-23 01:33:29 -0400

    NoMoreBedTime initial release

--- Mods/NoMoreBedTime/NoMoreBedTimeMod.cs
+++ Mods/NoMoreBedTime/NoMoreBedTimeMod.cs
@@ -1,34 +1,29 @@
 ﻿using HarmonyLib;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace NoMoreBedTime {
     [Serializable]
-    public class NoMoreBedTimeMod : Mod
-    {
-        public override void Load()
-        {
+    public class NoMoreBedTimeMod : Mod {
+        public override void Load() {
             Harmony harmony = new Harmony("NoMoreBedTime");
             harmony.PatchAll();
-
-            UnityEngine.MonoBehaviour.print("NoMoreBedTime loaded");
         }
 
-        public override void Start()
-        {
+        public override void Start() {
         }
 
-        public override void Update()
-        {
+        public override void Update() {
         }
     }
 
     [HarmonyPatch(typeof(NeedsInteractionController), "GetInteractionProposal")]
-    public static class NeedsInteractionControllerPatch
-    {
-        public static void Postfix(NeedsInteractionController __instance, HumanAI human, ref InteractionInfo __result)
-        {
+    public static class NeedsInteractionControllerPatch {
+        public static void Postfix(NeedsInteractionController __instance, HumanAI human, ref InteractionInfo __result) {
             if (__result != null && (
-                (__result.interaction == Interaction.Sleep &&  human.energy >= 0.15f) || 
+                (__result.interaction == Interaction.Sleep && human.energy >= 0.15f) ||
                 (__result.interaction == Interaction.SleepOnGround && human.energy > 0)))
 
                 __result = null;
